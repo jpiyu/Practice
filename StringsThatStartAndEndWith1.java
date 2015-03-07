a36968a6b881d9debfffe36235a35f982383a9fe
Piyush Joshi <joshipiyush217@gmail.com>
2015-03-07 12:36:14 +0530

    adding the code for finding the longest string starting and ending with 1

--- StringsThatStartAndEndWith1.java
+++ StringsThatStartAndEndWith1.java
@@ -7,5 +7,47 @@ public class StringsThatStartAndEndWith1 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("enter a binary string");
 		String str = br.readLine();
+		int arr[][] = new int[str.length()][str.length()];
+		/*for(int i=1;i<arr.length;i++)
+		{
+			arr[i][0] = 0;
+		}*/
+		if(str.charAt(0) == '1')
+		{
+			arr[0][0] = 1;
+		}
+		else
+		{
+			arr[0][0] = 0;
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=1;j<arr[i].length;j++)
+			{
+				if(j == 0)
+				{
+					arr[i][j] = 0;
+					continue;
+				}
+				if(str.charAt(j) == '1')
+				{
+					arr[i][j] = arr[i][j-1]+1;
+				}
+				else
+				{
+					arr[i][j] = arr[i][j-1];
+				}
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 }
