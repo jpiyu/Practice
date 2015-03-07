ad2c81538fc0eb584859f6574341a8b028e46e8a
Piyush Joshi <joshipiyush217@gmail.com>
2015-03-07 17:48:46 +0530

    adding program to find length of string that start and end with 1

--- StringsThatStartAndEndWith1.java
+++ StringsThatStartAndEndWith1.java
@@ -12,42 +12,61 @@ public class StringsThatStartAndEndWith1 {
 		{
 			arr[i][0] = 0;
 		}*/
-		if(str.charAt(0) == '1')
-		{
-			arr[0][0] = 1;
-		}
-		else
-		{
-			arr[0][0] = 0;
-		}
+		int record[][] = new int[arr.length][arr[0].length];
+		int lastOne = 0;
 		for(int i=0;i<arr.length;i++)
 		{
-			for(int j=1;j<arr[i].length;j++)
+			for(int j=i;j<arr[0].length;j++)
 			{
-				if(j == 0)
-				{
-					arr[i][j] = 0;
-					continue;
-				}
-				if(str.charAt(j) == '1')
-				{
-					arr[i][j] = arr[i][j-1]+1;
-				}
-				else
-				{
-					arr[i][j] = arr[i][j-1];
-				}
+				arr[i][j] = (int)(str.charAt(j) - '0');
 			}
 		}
-		
 		for(int i=0;i<arr.length;i++)
 		{
-			for(int j=0;j<arr[i].length;j++)
+			for(int j=0;j<arr[0].length;j++)
 			{
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
-		
+		if(arr[0][0] == 1)
+		{
+			record[0][0] = 1;
+			lastOne = 1;
+		}
+		for(int i=1;i<arr.length;i++)
+		{
+			record[i][0] = 0;
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=1;j<arr[0].length;j++)
+			{
+				if(arr[i][j] == 1)
+				{
+					if(lastOne != 0)
+					{
+						record[i][j] = j+2-lastOne;
+					}
+					else
+					{
+						lastOne = j+1;
+						record[i][j] = 1;
+					}
+				}
+				else if(arr[i][j] == 0)
+				{
+					record[i][j] = record[i][j-1];
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[0].length;j++)
+			{
+				System.out.print(record[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
